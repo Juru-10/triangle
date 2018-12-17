ddcd0880dfcce19a188506e9eaff8adcc0d0017b
Juru-10 <jurassu10@gmail.com>
2018-12-17 15:53:04 +0200

    add other variables declaration.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ function myFunction()
   var fv=document.getElementById("fv").value;
   var sv=document.getElementById("sv").value;
   var tv=document.getElementById("tv").value;
+  var msg1="Your triangle is ";
+  var msg2=" triangle.";
